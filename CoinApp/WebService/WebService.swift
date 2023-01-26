60d66f1e90f4fa5a82b86f16f27bae3f966a0402
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-01-26 15:20:07 +0300

    Get a all rates for TRY

--- CoinApp/WebService/WebService.swift
+++ CoinApp/WebService/WebService.swift
@@ -26,15 +26,12 @@ class WebServiceforCoinInfo {
                 } catch {
                     print(error)
                 }
-                
             }
         }.resume()
-        
-        
-        
     }
 }
 
+// MARK: for icon
 class WebServiceforCoinIcon {
     
     func getData(url: URL, completion: @escaping ([CoinIcon]) -> Void) {
@@ -56,8 +53,52 @@ class WebServiceforCoinIcon {
                 
             }
         }.resume()
-        
-        
-        
+
     }
+}
+
+class WebServiceforNewCoinInfo {
+    
+//    func getLastCoins() {
+//        guard let url = URL(string: "https://rest.coinapi.io/v1/exchangerate/TRY?apikey=E361CADD-258E-4ACE-B955-5A1E6F6F7EC9") else { return }
+//
+//        let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
+//            if let error = error {
+//                print(error)
+//            }
+//            guard let jsonData = data else { return }
+//
+//            let decoder = JSONDecoder()
+//            do {
+//                let decodedData = try decoder.decode(Coin.self, from: jsonData)
+//                print(decodedData)
+//            } catch {
+//                print(error.localizedDescription)
+//            }
+//        }
+//        dataTask.resume()
+//    }
+    
+    func getData(url: URL, completion: @escaping (Coin) -> Void) {
+
+        let request = URLRequest(url: url)
+        let dataTask = URLSession.shared.dataTask(with: request) { data, _, error in
+
+            if let error = error {
+                print("Error 1 : \(error.localizedDescription)")
+                return
+            }
+
+            guard let jsonData = data else { return }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(Coin.self, from: jsonData)
+                completion(decodedData)
+            } catch {
+                print("Error 2 : \(error)")
+            }
+        }
+        dataTask.resume()
+   }
+  
 }
